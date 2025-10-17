1ea936dc65b86733df435f9246849d0eb557dfd6
Joshua Paine <joshua@letterblock.com>
2025-10-17 17:02:00 -0400

    bugfix: Field type: :array write_method

--- lib/model_form_facade.rb
+++ lib/model_form_facade.rb
@@ -172,7 +172,7 @@ module ModelFormFacade
       write_method =
         case field.type
         in :object then ->(value) { object.send(write, field.form.as_written(value)) }
-        in :array then ->(value) { object.send(write, value.map { |v| field.form.as_written(v) }) }
+        in :array then ->(value) { object.send(write, value.values.map { |v| field.form.as_written(v) }) }
         in :scalar then ->(value) { object.send(write, value) }
         end
       field_methods_module.define_method(:"#{name}=", write_method)
